5b13bcda1c112b0cc0617e5b88007871f844684b
Wyatt Saint <wmsaint17@gmail.com>
2021-11-30 23:52:13 -0500

    Made code more OOP-like.  Added SQL functionality instead of .txt

--- src/main/java/webScraper/webScraper/App.java
+++ src/main/java/webScraper/webScraper/App.java
@@ -1,12 +1,9 @@
 package webScraper.webScraper;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.time.LocalDateTime;
@@ -18,87 +15,104 @@ import org.jsoup.select.Elements;
 
 public class App {
 
-	public static void main(String[] args) {
-		
-		try {
-			createDatabase();
-		} catch (ClassNotFoundException | SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-	}
-	
-	static void createDatabase() throws ClassNotFoundException, SQLException {
-		
-		
-		Connection conn = null;
-        Statement stmt = null;
-            //STEP 2: Register JDBC driver
-        	Class.forName("org.mariadb.jdbc.Driver");
-            
-            //STEP 3: Open a connection
-            System.out.println("Connecting to a selected database...");
-            conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/", "root", "root");
-            System.out.println("Connected database successfully...");
-            stmt = conn.createStatement();
-            String sql = "create database if not exists webScraper";
-            stmt.executeUpdate(sql);
-		
-	}
-	
-	static void webScrape() {
-		
-		try {
-			String url = "https://www.health.pa.gov/topics/disease/coronavirus/Pages/Cases.aspx";
-			Document doc = Jsoup.connect(url).get();
-			Elements info = doc.getElementsByTag("strong");
-			
-			
-			
-			FileWriter fw = new FileWriter("data");
-			fw.write(info.toString());
-			fw.close();
-			
-			String caseCount = Files.readAllLines(Paths.get("data")).get(3);
-			
-			String caseCount1 = caseCount.replaceFirst("<strong>", "");
-			String caseCount2 = caseCount1.substring(0, caseCount1.length()-9);
-			String caseCount3 = caseCount2.replaceAll(",", "");
-			
-			int actualCaseCount = Integer.parseInt(caseCount3.toString());
+	static Connection conn = null;
+	static Statement stmt = null;
+	static String sql;
+	static String date;
+	static String currentNumbers;
+	static String lastRecord;
+	static String dataShift;
 
-			
-			String oldCaseCount = Files.readAllLines(Paths.get("caseCountFile")).get(0);
-			int newOldCaseCount = Integer.valueOf(oldCaseCount.toString());
-			
-			BufferedWriter bw = new BufferedWriter(new FileWriter("caseCountFile"));
-			bw.write(caseCount3);
-			bw.flush();
-			
-			int caseTrend = actualCaseCount - newOldCaseCount;
-			
-			DateTimeFormatter dtf = DateTimeFormatter.ofPattern("uuuu/MM/dd HH:mm:ss");
-			LocalDateTime now = LocalDateTime.now();
-			String dateTime = dtf.format(now);
-			
-			BufferedWriter caseTrends = new BufferedWriter(new FileWriter("caseTrendFile", true));
-			caseTrends.newLine();
-			caseTrends.write(new Integer(caseTrend).toString());
-			caseTrends.newLine();
-			caseTrends.write(dateTime);
-			caseTrends.newLine();
-			caseTrends.write("------");
-			caseTrends.flush();
-			caseTrends.close();
-			
-			System.out.println(caseTrend);
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	public static void main(String[] args) throws IOException, SQLException, ClassNotFoundException {
+
+		
+		getLastRecord();
+		createDatabaseAndTables();
+		getCurrentDate();
+		scrapeData();
+		insertCurrentDataIntoTable();
+		dataShift();
+		insertDataShiftIntoTable();
+		System.out.println(dataShift);
 		
 	}
+
+	static void createDatabaseAndTables() throws ClassNotFoundException, SQLException {
+
+		Class.forName("org.mariadb.jdbc.Driver");
+		conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/", "root", "root");
+		stmt = conn.createStatement();
+		sql = "create database if not exists webScraper";
+		stmt.executeUpdate(sql);
+		conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/webScraper", "root", "root");
+		stmt = conn.createStatement();
+		sql = "create table if not exists totalCases (case_change int)";
+		stmt.executeUpdate(sql);
+		conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/webScraper", "root", "root");
+		stmt = conn.createStatement();
+		sql = "create or replace table currentCases (Current_Cases int)";
+		stmt.executeUpdate(sql);
+
+	}
+
+	static void getCurrentDate() {
+
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("uuuu/MM/dd");
+		LocalDateTime now = LocalDateTime.now();
+		date = dtf.format(now);
+
+	}
+
+	static void scrapeData() throws IOException {
+
+		String url = "https://www.health.pa.gov/topics/disease/coronavirus/Pages/Cases.aspx";
+		Document doc = Jsoup.connect(url).get();
+		Elements info = doc.getElementsByTag("strong");
+		currentNumbers = (String.valueOf(info)).substring(87, 96).replaceAll(",", "");
+
+	}
+
+	static void insertDataShiftIntoTable() throws SQLException {
+		
+		int insert = Integer.valueOf(dataShift);
+
+		conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/webScraper", "root", "root");
+		stmt = conn.createStatement();
+		sql = "insert into totalcases (case_change, date) VALUES " + "('" + Integer.valueOf(dataShift) + "', '" + date + "')";
+		stmt.executeUpdate(sql);
+	}
 	
+	static void insertCurrentDataIntoTable() throws SQLException {
+		
+		conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/webScraper", "root", "root");
+		stmt = conn.createStatement();
+		sql = "insert into currentCases (Current_Cases) VALUES " + "(" + Integer.valueOf(currentNumbers) + ")";
+		stmt.executeUpdate(sql);
+
+	}
+
+	static void getLastRecord() throws ClassNotFoundException, SQLException {
+
+		Class.forName("org.mariadb.jdbc.Driver");
+		conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/webScraper", "root", "root");
+		stmt = conn.createStatement();
+		ResultSet rs;
+		sql = "select * from currentCases";
+		rs = stmt.executeQuery(sql);
+		if (rs.next()) {
+			int cases = rs.getInt("Current_Cases");
+			lastRecord = String.valueOf(cases);
+		}
+
+	}
+	
+	static void dataShift() {
+		
+		int currentNumbersconvert = Integer.valueOf(currentNumbers);
+		int lastRecordConvert = Integer.valueOf(lastRecord);
+		int result = currentNumbersconvert - lastRecordConvert;
+		dataShift = String.valueOf(result);
+		
+	}
+
 }
