59f72e54e80a8f7788408595b770edc60756afc8
Wyatt Saint <wmsaint17@gmail.com>
2021-12-02 15:35:10 -0500

    Added java swing component for gui

--- src/main/java/webScraper/webScraper/App.java
+++ src/main/java/webScraper/webScraper/App.java
@@ -19,13 +19,16 @@ public class App {
 	static Statement stmt = null;
 	static String sql;
 	static String date;
-	static String currentNumbers;
+	static String currentPACovidNumbers;
+	static String populationPA;
 	static String lastRecord;
 	static String dataShift;
+	static String percentRecovered;
+	static String peopleSickInPA;
+	static String percentageSick;
 
 	public static void main(String[] args) throws IOException, SQLException, ClassNotFoundException {
 
-		
 		getLastRecord();
 		createDatabaseAndTables();
 		getCurrentDate();
@@ -33,8 +36,11 @@ public class App {
 		insertCurrentDataIntoTable();
 		dataShift();
 		insertDataShiftIntoTable();
-		System.out.println(dataShift);
+		System.out.println("New cases since last runtime: " + dataShift);
+		numberOfPeopleSickInPA();
 		
+		WebScraperGui run = new WebScraperGui();
+
 	}
 
 	static void createDatabaseAndTables() throws ClassNotFoundException, SQLException {
@@ -65,28 +71,42 @@ public class App {
 
 	static void scrapeData() throws IOException {
 
-		String url = "https://www.health.pa.gov/topics/disease/coronavirus/Pages/Cases.aspx";
-		Document doc = Jsoup.connect(url).get();
-		Elements info = doc.getElementsByTag("strong");
-		currentNumbers = (String.valueOf(info)).substring(87, 96).replaceAll(",", "");
+		String url;
+		Document doc;
+		Elements info;
+
+		url = "https://www.health.pa.gov/topics/disease/coronavirus/Pages/Cases.aspx";
+		doc = Jsoup.connect(url).get();
+		info = doc.getElementsByTag("strong");
+		currentPACovidNumbers = (String.valueOf(info)).substring(87, 96).replaceAll(",", "");
+		url = "https://www.census.gov/quickfacts/fact/table/PA#";
+		doc = Jsoup.connect(url).get();
+		info = doc.getElementsByAttribute("data-value");
+		populationPA = (String.valueOf(info).substring(51, 59));
+		url = "https://www.health.pa.gov/topics/disease/coronavirus/Pages/Cases.aspx";
+		doc = Jsoup.connect(url).get();
+		info = doc.getElementsByTag("strong");
+		percentRecovered = (String.valueOf(info).substring(220, 222));
+		
 
 	}
 
 	static void insertDataShiftIntoTable() throws SQLException {
-		
+
 		int insert = Integer.valueOf(dataShift);
 
 		conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/webScraper", "root", "root");
 		stmt = conn.createStatement();
-		sql = "insert into totalcases (case_change, date) VALUES " + "('" + Integer.valueOf(dataShift) + "', '" + date + "')";
+		sql = "insert into totalcases (case_change, date) VALUES " + "('" + Integer.valueOf(dataShift) + "', '" + date
+				+ "')";
 		stmt.executeUpdate(sql);
 	}
-	
+
 	static void insertCurrentDataIntoTable() throws SQLException {
-		
+
 		conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/webScraper", "root", "root");
 		stmt = conn.createStatement();
-		sql = "insert into currentCases (Current_Cases) VALUES " + "(" + Integer.valueOf(currentNumbers) + ")";
+		sql = "insert into currentCases (Current_Cases) VALUES " + "(" + Integer.valueOf(currentPACovidNumbers) + ")";
 		stmt.executeUpdate(sql);
 
 	}
@@ -105,13 +125,32 @@ public class App {
 		}
 
 	}
-	
+
 	static void dataShift() {
-		
-		int currentNumbersconvert = Integer.valueOf(currentNumbers);
+
+		int currentPACovidNumbersconvert = Integer.valueOf(currentPACovidNumbers);
 		int lastRecordConvert = Integer.valueOf(lastRecord);
-		int result = currentNumbersconvert - lastRecordConvert;
+		int result = currentPACovidNumbersconvert - lastRecordConvert;
 		dataShift = String.valueOf(result);
+
+	}
+	
+	static void numberOfPeopleSickInPA() {
+		
+		double percent =  Integer.valueOf(percentRecovered) * .01;
+		double percentNotRecovered = 1 - percent;
+		double numberOfPeopleNotRecovered = percentNotRecovered * Integer.valueOf(currentPACovidNumbers);
+		int numberOfPeopleNotRecovered1 = (int) numberOfPeopleNotRecovered;
+		peopleSickInPA = String.valueOf(numberOfPeopleNotRecovered1);
+		System.out.println("People currently sick: " + peopleSickInPA);
+		
+		double percentageSick0 = Integer.valueOf(peopleSickInPA);
+		double percentageSick01 = Integer.valueOf(populationPA);
+		double percentageSick001 = percentageSick0 / percentageSick01;
+		double percentageSick1 = percentageSick001 * 100;
+		percentageSick = String.valueOf((int) percentageSick1);
+		System.out.println("Percentage of population currently sick: " + percentageSick + "%");
+		
 		
 	}
 
---
+++ src/main/java/webScraper/webScraper/WebScraperGui.java
@@ -0,0 +1,79 @@
+package webScraper.webScraper;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.ScrollPane;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+public class WebScraperGui extends JFrame implements ActionListener {
+	
+	static JFrame myFrame;
+	static JPanel myPanel;
+	static JTable myTable;
+
+	WebScraperGui() {
+		
+		myFrame = new JFrame();
+		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		myFrame.setLayout(new FlowLayout(FlowLayout.CENTER));
+		myFrame.setTitle("COVID Web Scraper");
+		myFrame.setPreferredSize(new Dimension(1100, 690));
+		myFrame.setResizable(false);
+		
+		myPanel = new JPanel();
+		myPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+		myPanel.setPreferredSize(new Dimension(1000, 650));
+		myPanel.setBackground(Color.GRAY);
+		
+		createJTable();
+		myTable.setPreferredSize(new Dimension(1000, 600));
+		myTable.setFont(new Font("Arial", Font.PLAIN, 60));
+		myTable.setRowHeight(myTable.getRowHeight() + 50);
+		myTable.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
+		myTable.setShowGrid(true);
+		
+		
+		
+		myPanel.add(myTable);
+		myFrame.add(myPanel);
+		
+		myFrame.pack();
+		myFrame.setLocationRelativeTo(null);
+		myFrame.setVisible(true);
+		
+		
+	}
+	
+	
+	
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	static void createJTable() {
+		
+		String [][] data = {
+
+				{"New cases since last runtime:", App.dataShift},
+				{"People currently sick:", App.peopleSickInPA},
+				{"Percent of population currently sick:", App.percentageSick}
+		};
+		String [] header = {"Description", "Data"};
+		DefaultTableModel model = new DefaultTableModel(data, header);
+		myTable = new JTable(model);
+		
+	}
+
+}
